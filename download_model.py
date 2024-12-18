33b456984261b59ead8c2ca378dd6ff4df6fdc3f
Syed Mohamad Tawseeq <theguysta56@gmail.com>
2024-12-18 11:48:47 +0530

    updated download_model.py

--- download_model.py
+++ download_model.py
@@ -3,20 +3,15 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 #from transformers import AutoProcessor, PaliGemmaForConditionalGeneration
 from transformers import AutoProcessor, AutoModelForImageTextToText
-# Define model name and directory to save it
 #model_name = 'microsoft/phi-2'
 #model_id = "google/paligemma-3b-mix-224"
-model_save_dir = '/scratch/22ch10090/models'  # Path to save the model
+model_save_dir = '/scratch/22ch10090/models'  
 
-# Ensure the directory exists
 os.makedirs(model_save_dir, exist_ok=True)
 
-# Download model and tokenizer
-#print(f"Downloading model {model_id}...")
 processor = AutoProcessor.from_pretrained("facebook/chameleon-7b")
 model = AutoModelForImageTextToText.from_pretrained("facebook/chameleon-7b")
 
-# Save the model and tokenizer locally
 model.save_pretrained(model_save_dir)
 processor.save_pretrained(model_save_dir)
 
